3d816400002cb93497d72726e3905ec9e42c6b08
Alexander Zemtsov <alexander.zemtsov@ru.ibm.com>
2021-04-15 11:57:12 +0300

    small fixes

--- chaincode_query_installed.sh
+++ chaincode_query_installed.sh
@@ -4,6 +4,9 @@
 
 PATH=${PWD}/bin:$PATH
 
+CC_VERSION=${1:-"1"}
+CC_SEQUENCE=${2:-1}
+
 export FABRIC_CFG_PATH=${PWD}/config
 
 
@@ -11,26 +14,24 @@ export FABRIC_CFG_PATH=${PWD}/config
 # ORG 1
 ######################################################################
 
-echo
-echo "Querying chaincode ${CC_NAME} from ORG 1"
-
 export CORE_PEER_LOCALMSPID=Org1MSP
 export CORE_PEER_ID=peer0.org1.example.com
 export CORE_PEER_ADDRESS=localhost:7051
 export CORE_PEER_MSPCONFIGPATH=${PEER0_ORG1_MSP}
 export CORE_PEER_TLS_ROOTCERT_FILE=${PEER0_ORG1_CA}
 
-peer chaincode query -C "$CHANNEL_NAME" -n "${CC_NAME}" -c '{"Args": ["read", "value1"]}' >&log.txt
+echo
+echo "Querying the list of installed chaincodes on ${CORE_PEER_ID}"
 
+peer lifecycle chaincode queryinstalled >&log.txt
 cat log.txt
+PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
 
+echo "Package ID is ${PACKAGE_ID}"
 
 ######################################################################
 # ORG 2
 ######################################################################
-
-echo
-echo "Querying chaincode ${CC_NAME} from ORG 2"
 
 export CORE_PEER_LOCALMSPID=Org2MSP
 export CORE_PEER_ID=peer0.org2.example.com
@@ -38,15 +39,11 @@ export CORE_PEER_ADDRESS=localhost:9051
 export CORE_PEER_MSPCONFIGPATH=${PEER0_ORG2_MSP}
 export CORE_PEER_TLS_ROOTCERT_FILE=${PEER0_ORG2_CA}
 
-peer chaincode query -C "$CHANNEL_NAME" -n "${CC_NAME}" -c '{"Args": ["read", "value1"]}' >&log.txt
-
-#docker exec \
-#    -e "CORE_PEER_LOCALMSPID=Org2MSP" \
-#    -e "CORE_PEER_ID=peer0.org2.example.com" \
-#    -e "CORE_PEER_ADDRESS=peer0.org2.example.com:9051" \
-#    -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp" \
-#    cli \
-#    peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"Args": ["read", "value1"]}' >&log.txt
-
+echo
+echo "Querying the list of installed chaincodes on ${CORE_PEER_ID}"
+peer lifecycle chaincode queryinstalled >&log.txt
 cat log.txt
+PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+
+echo "Package ID is ${PACKAGE_ID}"
 
--- env.sh
+++ env.sh
@@ -26,4 +26,4 @@ export CC_NAME=hello
 export CC_RUNTIME_LANGUAGE=golang
 export CC_SRC_PATH=chaincode/${CC_NAME}
 export INIT_REQUIRED="--init-required"
-export CC_END_POLICY="--signature-policy OR('Org1MSP.peer','Org2MSP.peer')"
+export CC_END_POLICY="--signature-policy AND('Org1MSP.peer','Org2MSP.peer')"
